7902c020a91a88329e08147c28330925d788a09b
RPI News <44128563+mobilegmYT@users.noreply.github.com>
2021-04-25 14:42:19 -0600

    stupid deb

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 curl -s --compressed "https://mcpirevival.tk/mcpi-packages/debs/KEY.gpg" | sudo apt-key add -
-sudo curl -s --compressed -o /etc/apt/sources.list.d/mcpi-revival.list "deb https://mcpirevival.tk/mcpi-packages/debs/mcpi-revival.list"
+sudo curl -s --compressed -o /etc/apt/sources.list.d/mcpi-revival.list "https://mcpirevival.tk/mcpi-packages/debs/mcpi-revival.list"
 sudo apt update
