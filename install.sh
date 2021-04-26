3273faf1ef3f3d9e25c597267f4a9fff17fdd05a
RPI News <44128563+mobilegmYT@users.noreply.github.com>
2021-04-25 20:10:27 -0600

    Switch to wget

--- install.sh
+++ install.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
-curl -s --compressed "https://mcpirevival.tk/mcpi-packages/debs/KEY.gpg" | sudo apt-key add -
-sudo curl -s --compressed -o /etc/apt/sources.list.d/mcpi-revival.list "https://mcpirevival.tk/mcpi-packages/debs/mcpi-revival.list"
-sudo apt update
+function error() {
+  echo -e "$(tput setaf 1)$(tput bold)$1$(tput sgr 0)"
+  exit 1
+}
+
+function warning() {
+  echo -e "$(tput setaf 1)$(tput bold)$1$(tput sgr 0)"
+  sleep 2
+  true
+}
+
+wget -qO- https://mcpirevival.tk/mcpi-packages/debs/KEY.gpg | sudo apt-key add - || error "Failed to download and add key!"
+sudo wget https://mcpirevival.tk/mcpi-packages/debs/mcpi-revival.list -O /etc/apt/sources.list.d/mcpi-revival.list || error "Failed to download 'mcpi-revival.list'!"
+sudo apt update || warning "Failed to run 'sudo apt update'! Please run that command manually"
