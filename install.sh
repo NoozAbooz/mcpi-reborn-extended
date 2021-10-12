eac35da2ea6974334ff1e9516b449049019d966c
RPI News <44128563+mobilegmYT@users.noreply.github.com>
2021-10-11 18:50:36 -0600

    fix script

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ function warning() {
   true
 }
 
-sudo wget -q -O /etc/apt/trusted.gpg.d/mcpi-packages-buster.gpg https://github.com/mobilegmYT/mcpi-packages-buster/raw/main/debs/KEY.gpg || error "Failed to download and add key!"
-echo "deb [trusted=yes] https://mobilegmyt.github.io/mcpi-packages-buster/ /" | sudo tee /etc/apt/sources.list.d/mcpi-packages-buster.list || error "Failed to download 'mcpi-revival.list'!"
+wget -qO- https://github.com/mobilegmYT/mcpi-packages-buster/raw/main/debs/KEY.gpg | sudo apt-key add - || error "Failed to download and add key!"
+echo "deb [trusted=yes] https://mobilegmyt.github.io/mcpi-packages-buster/debs/ /" | sudo tee /etc/apt/sources.list.d/mcpi-packages-buster.list || error "Failed to download 'mcpi-revival.list'!"
 
 sudo apt update --allow-releaseinfo-change || warning "Failed to run 'sudo apt update'! Please run that command manually"
