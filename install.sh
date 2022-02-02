fdbad5eccfc6864dd65652cb6b807625f5d5f2b4
RPI News <44128563+mobilegmYT@users.noreply.github.com>
2022-02-01 21:01:43 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,35 +12,16 @@ function warning() {
   true
 }
 
-function install-depends() {
-  if [ $(dpkg-query -W -f='${Status}' wget 2>/dev/null | grep -c "ok installed") -eq 0 ]; 
-  then
-    sudo apt-get install -y wget
-  fi 
+# Install depends
+if [ $(dpkg-query -W -f='${Status}' wget 2>/dev/null | grep -c "ok installed") -eq 0 ]; 
+then
+  sudo apt-get install -y wget
+fi 
   
-  if [ $(dpkg-query -W -f='${Status}' gnupg2 2>/dev/null | grep -c "ok installed") -eq 0 ];
-  then
-    sudo apt-get install gnupg2
-  fi
-}
-
-function install-api() {
-  if [ $(dpkg-query -W -f='${Status}' python3-minecraftpi 2>/dev/null | grep -c "ok installed") -eq 0 ];
-  then
-    sudo apt-get install -y python3-minecraftpi
-  fi 
-  
-  wget https://gist.github.com/mobilegmYT/78f50d3b80924d0c18ed818552254695/raw/a80ead7d30edf16327622002466f7b7e7df69aa6/minecraft.py
-  sudo mv minecraft.py /usr/lib/python3/dist-packages/mcpi/minecraft.py
-}
-
-# Install depends if not already installed
-read -p "Install depends wget and gnupg2? (y/n) -> " choice
-case "$choice" in 
-  y|Y ) install-depends;;
-  n|N ) echo "Need depends to install! Exiting..." && exit 1;;
-  * ) echo "invalid";;
-esac
+if [ $(dpkg-query -W -f='${Status}' gnupg2 2>/dev/null | grep -c "ok installed") -eq 0 ];
+then
+  sudo apt-get install gnupg2
+fi
 
 # Install repo
 sudo wget -q -O /etc/apt/trusted.gpg.d/mcpi-reborn-extended.gpg https://github.com/mobilegmYT/mcpi-packages-extended/raw/main/debs/KEY.gpg
@@ -49,18 +30,19 @@ wget -qO- https://github.com/mobilegmYT/mcpi-packages-extended/raw/main/debs/mcp
 sudo apt update --allow-releaseinfo-change || warning "Failed to run 'sudo apt update'! Please run that command manually"
 
 # Nuke vanilla reborn if installed
-if [ $(dpkg-query -W -f='${Status}' minecraft-pi-reborn-client 2>/dev/null | grep -c "ok installed") -eq 0 ];
+if [ $(dpkg-query -W -f='${Status}' minecraft-pi-reborn-client 2>/dev/null | grep -c "ok installed") -eq 1 ];
 then
   sudo apt-get uninstall -y minecraft-pi-reborn-client && sudo apt-get install -y minecraft-pi-reborn-client || warning "Could not reinstall reborn to switch to extended version! Please do it manually."
 fi
 
-# Install modified python lib
-read -p "Install MCPI python API? (y/n) -> " choice
-case "$choice" in 
-  y|Y ) install-api;;
-  n|N ) break;;
-  * ) echo "invalid";;
-esac
+# Install Python API
+if [ $(dpkg-query -W -f='${Status}' python3-minecraftpi 2>/dev/null | grep -c "ok installed") -eq 0 ];
+then
+  sudo apt-get install -y python3-minecraftpi
+fi 
+  
+wget https://gist.github.com/mobilegmYT/78f50d3b80924d0c18ed818552254695/raw/a80ead7d30edf16327622002466f7b7e7df69aa6/minecraft.py
+sudo mv minecraft.py /usr/lib/python3/dist-packages/mcpi/minecraft.py
 
 # Install sound
 wget https://archive.org/download/libminecraftpe0.6.1/libminecraftpe06%2B08.so || warning "Failed to download sound files!"
