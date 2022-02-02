dd913630069bcdcf751509d0dafad5d2ba87c466
RPI News <44128563+mobilegmYT@users.noreply.github.com>
2022-02-01 19:31:04 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -35,13 +35,12 @@ function install-api() {
 }
 
 # Install depends if not already installed
-read -r -p "Install dependencies wget and gnupg2? [y/N] " response
-if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
-then
-  install-depends
-else
-  echo "Need depends to install! Exiting..." && exit 1
-fi
+read -p "Install depends 'wget' and 'gnupg'? (y/n)" choice
+case "$choice" in 
+  y|Y ) install-depends;;
+  n|N ) echo "Need depends to install! Exiting..." && exit 1;;
+  * ) echo "invalid";;
+esac
 
 # Install repo
 wget -qO- https://github.com/mobilegmYT/mcpi-packages-extended/raw/main/debs/KEY.gpg | sudo apt-key add - || error "Failed to download and add key!"
@@ -56,19 +55,14 @@ then
 fi
 
 # Install modified python lib
-read -p " (y/n) " -n 1 -r
-echo    # (optional) move to a new line
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-  install-api
-fi
+read -p "Install MCPI python API? (y/n)" choice
+case "$choice" in 
+  y|Y ) install-api;;
+  n|N ) break;;
+  * ) echo "invalid";;
+esac
 
-read -r -p "Install MCPI python API? [y/N] " response
-if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
-then
-    install-api()
-fi
-
+# Install sound
 wget https://archive.org/download/libminecraftpe0.6.1/libminecraftpe06%2B08.so || warning "Failed to download sound files!"
 mkdir -p ~/.minecraft-pi/overrides
 mv libminecraftpe06+08.so ~/.minecraft-pi/overrides/libminecraftpe.so
