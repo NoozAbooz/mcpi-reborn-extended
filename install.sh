78b89a963a743c4df8ea09c7ab97376e91228019
RPI News <44128563+mobilegmYT@users.noreply.github.com>
2022-02-01 20:55:06 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -43,7 +43,7 @@ case "$choice" in
 esac
 
 # Install repo
-wget -qO- https://github.com/mobilegmYT/mcpi-packages-extended/raw/main/debs/KEY.gpg && sudo mv KEY.gpg /etc/apt/trusted.gpg.d/mcpi-reborn-extended.gpg  || error "Failed to download and add key!"
+sudo wget -q -O /etc/apt/trusted.gpg.d/mcpi-reborn-extended.gpg https://github.com/mobilegmYT/mcpi-packages-extended/raw/main/debs/KEY.gpg || error "Failed to download and add key!"
 wget -qO- https://github.com/mobilegmYT/mcpi-packages-extended/raw/main/debs/mcpi-revival.list | sudo tee /etc/apt/sources.list.d/mcpi-reborn-extended.list || error "Failed to download 'mcpi-revival.list'!"
 
 sudo apt update --allow-releaseinfo-change || warning "Failed to run 'sudo apt update'! Please run that command manually"
@@ -51,7 +51,7 @@ sudo apt update --allow-releaseinfo-change || warning "Failed to run 'sudo apt u
 # Nuke vanilla reborn if installed
 if [ $(dpkg-query -W -f='${Status}' minecraft-pi-reborn-client 2>/dev/null | grep -c "ok installed") -eq 0 ];
 then
-  sudo apt-get uninstall -y minecraft-pi-reborn-client && sudo apt-get install minecraft-pi-reborn-client || warning "Could not reinstall reborn to switch to extended version! Please do it manually."
+  sudo apt-get uninstall -y minecraft-pi-reborn-client && sudo apt-get install -y minecraft-pi-reborn-client || warning "Could not reinstall reborn to switch to extended version! Please do it manually."
 fi
 
 # Install modified python lib
