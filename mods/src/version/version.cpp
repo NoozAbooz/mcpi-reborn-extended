25b4b023620918f5b48d1d7893d9ff6742cecb3f
RPI News <44128563+mobilegmYT@users.noreply.github.com>
2022-02-04 21:27:10 -0700

    Update version.cpp

--- mods/src/version/version.cpp
+++ mods/src/version/version.cpp
@@ -9,7 +9,7 @@ char *version_get() {
     static char *version = NULL;
     // Load
     if (version == NULL) {
-        safe_asprintf(&version, "MCPI++ v%s", *minecraft_pi_version, VERSION);
+        safe_asprintf(&version, "MCPI-Reborn Extended v%s", *minecraft_pi_version, VERSION);
     }
     // Return
     return version;
