cc1553d2597d5edbcd6278f486e9ad734b492ff7
Michael Zheng <44128563+mobilegmYT@users.noreply.github.com>
2022-03-06 14:11:31 -0700

    Add custom skins and y/n prompts

--- install.sh
+++ install.sh
@@ -13,23 +13,15 @@ function warning() {
 }
 
 # Install depends
-if [ $(dpkg-query -W -f='${Status}' wget 2>/dev/null | grep -c "ok installed") -eq 0 ]; 
+if [ $(dpkg-query -W -f='${Status}' wget 2>/dev/null | grep -c "ok installed") -eq 0 ] || [ $(dpkg-query -W -f='${Status}' gnupg2 2>/dev/null | grep -c "ok installed") -eq 0 ] || [ $(dpkg-query -W -f='${Status}' unzip 2>/dev/null | grep -c "ok installed") -eq 0 ] || [ $(dpkg-query -W -f='${Status}' zenity 2>/dev/null | grep -c "ok installed") -eq 0 ];
 then
-  sudo apt-get install -y wget
-fi 
-  
-if [ $(dpkg-query -W -f='${Status}' gnupg2 2>/dev/null | grep -c "ok installed") -eq 0 ];
-then
-  sudo apt-get install gnupg2
-fi
-
-if [ $(dpkg-query -W -f='${Status}' unzip 2>/dev/null | grep -c "ok installed") -eq 0 ];
-then
-  sudo apt-get install unzip
+  echo -e "\e[4m\e[21m\e[5mInstalling dependencies...\e[0m\e[97m"
+  sudo apt install -y wget gnupg unzip zenity || error "Failed to install dependencies, see error above"
 fi
 
 # Install repo
-sudo wget -q -O /etc/apt/sources.list.d/mcpi-reborn-extended.list https://github.com/mobilegmYT/mcpi-reborn-extended/raw/main/debs/mcpi-reborn-extended.list
+echo -e "\e[4m\e[21m\e[5mInstalling APT repository...\e[0m\e[97m"
+sudo wget -qO- /etc/apt/sources.list.d/mcpi-reborn-extended.list https://github.com/mobilegmYT/mcpi-reborn-extended/raw/main/debs/mcpi-reborn-extended.list || error "Failed to download repo!"
 
 # Install GPG key
 sudo wget https://github.com/mobilegmYT/mcpi-reborn-extended/raw/main/debs/KEY.gpg
@@ -42,32 +34,72 @@ sudo apt update --allow-releaseinfo-change || warning "Failed to run 'sudo apt u
 # Nuke vanilla reborn if installed
 if [ $(dpkg-query -W -f='${Status}' minecraft-pi-reborn-client 2>/dev/null | grep -c "ok installed") -eq 1 ];
 then
-  sudo apt-get remove -y minecraft-pi-reborn-client || warning "Could not reinstall reborn to switch to extended version! Please do it manually."
+  echo -e "\e[4m\e[21m\e[5mRemoving vanilla reborn to resolve conflicts...\e[0m\e[97m"
+  sudo apt-get remove -y minecraft-pi-reborn-client || warning "Could not reinstall reborn to switch to extended version! Please do it manually"
+  sudo apt-get install -y minecraft-pi-reborn-client
 fi
-
-sudo apt-get install -y minecraft-pi-reborn-client
 
 # Install Python API
 if [ $(dpkg-query -W -f='${Status}' python3-minecraftpi 2>/dev/null | grep -c "ok installed") -eq 0 ];
 then
-  sudo apt-get install -y python3-minecraftpi
-fi 
-  
-wget https://gist.github.com/mobilegmYT/78f50d3b80924d0c18ed818552254695/raw/a80ead7d30edf16327622002466f7b7e7df69aa6/minecraft.py
-sudo mv minecraft.py /usr/lib/python3/dist-packages/mcpi/minecraft.py
+  echo "[OPTIONAL] Install Python API? (press '1' or '2' and then press [Enter] to select)"
+  select yn in "Yes" "No"; do
+    case $yn in
+      Yes ) echo -e "\e[4m\e[21m\e[5m[OPTIONAL] Installing Python API...\e[0m\e[97m" && sudo apt install -y python3-minecraftpi || warning "Could not install Python API" && wget https://gist.github.com/mobilegmYT/78f50d3b80924d0c18ed818552254695/raw/a80ead7d30edf16327622002466f7b7e7df69aa6/minecraft.py && sudo mv minecraft.py /usr/lib/python3/dist-packages/mcpi/minecraft.py;;
+      No ) break;;
+    esac
+  done
+fi
 
 # Install sound
-wget https://archive.org/download/libminecraftpe0.6.1/libminecraftpe06%2B08.so || warning "Failed to download sound files!"
-mkdir -p ~/.minecraft-pi/overrides
-mv libminecraftpe06+08.so ~/.minecraft-pi/overrides/libminecraftpe.so
+echo "[OPTIONAL] Enable sound?"
+select yn in "Yes" "No"; do
+  case $yn in
+    Yes ) echo -e "\e[4m\e[21m\e[5m[OPTIONAL] Installing sound files...\e[0m\e[97m" && wget https://archive.org/download/libminecraftpe0.6.1/libminecraftpe06%2B08.so && mkdir -p ~/.minecraft-pi/overrides && mv "libminecraftpe06+08.so" ~/.minecraft-pi/overrides/libminecraftpe.so;;
+    No ) break;;
+  esac
+done
 
 # Install 1.18 textures
-echo "The script will install a texturepack that updates the textures to MC Java 1.18! If you don't want this, run 'rm -rf ~/.minecraft-pi/overrides/images' after installation."
-sleep 5
-wget https://cdn.discordapp.com/attachments/740287938453045401/944751207644278864/converted.zip
-unzip converted.zip
-mkdir -p ~/.minecraft-pi/overrides/images
-mv converted/* ~/.minecraft-pi/overrides/images/
-rm -rf converted/ converted.zip
+function install_updated_textures {
+  wget https://cdn.discordapp.com/attachments/740287938453045401/944751207644278864/converted.zip
+  unzip converted.zip
+  mkdir -p ~/.minecraft-pi/overrides/images
+  mv converted/* ~/.minecraft-pi/overrides/images/
+  rm -rf converted/ converted.zip
+}
+
+echo "[OPTIONAL] Install MC 1.18 texturepack?"
+select yn in "Yes" "No"; do
+  case $yn in
+    Yes ) echo -e "\e[4m\e[21m\e[5m[OPTIONAL] Installing 1.18 texturepack...\e[0m\e[97m" && install_updated_textures;;
+    No ) break;;
+  esac
+done
+
+# Add custom skin
+function convert_skin {
+  pip install pillow
+  skinpath=$(zenity --file-selection --title='Select your custom Java/Bedrock skin file (must be 4px)' --file-filter='Picture files (png) | *.png' --file-filter='All files | *')
+  echo "Using image:" $skinpath
+  
+  cp $skinpath ~/.minecraft-pi/skin-mcpi.png
+  mkdir -p ~/.minecraft-pi/overrides/images/mob
+  
+  wget https://bitbucket.org/MattHawkinsUK/rpispy-misc/raw/master/minecraft/minecraft_skin_fixer.py
+  sed -i "s|/opt/minecraft-pi/data/images/mob/char.png|~/.minecraft-pi/skin-mcpi.png|g" minecraft_skin_fixer.py
+  python3 minecraft_skin_fixer.py
+  
+  mv ~/.minecraft-pi/skin-mcpi.png ~/.minecraft-pi/overrides/images/mob/char.png
+  rm minecraft_skin_fixer.py
+}
+
+echo "[OPTIONAL] Use custom skin?"
+select yn in "Yes" "No"; do
+  case $yn in
+    Yes ) echo -e "\e[4m\e[21m\e[5m[OPTIONAL] Adding custom skin...\e[0m\e[97m" && install_updated_textures;;
+    No ) break;;
+  esac
+done
 
 echo "Installation success! Launch it with 'minecraft-pi-reborn-client' and enjoy!"
