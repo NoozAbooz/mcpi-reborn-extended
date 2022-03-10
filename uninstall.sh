71bfa33cd5a8fd8ffda764c017f824171ea4b4eb
Sussy Inc <SussyS0S-Operating-Systems@protonmail.com>
2022-03-10 13:58:37 -0500

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-echo "This script will REMOVE the MCPI++ APT REPO! Press [Enter] to continue, or [Ctrl+C] to exit"
-
-read -p "Waiting for choice.."
+read -p $'This script will REMOVE the MCPI++ APT REPO! Press [Enter] to continue, or [Ctrl+C] to exit\x0a'
 
 sudo rm /etc/apt/sources.list.d/mcpi-reborn-extended.list
 sudo rm /etc/apt/trusted.gpg.d/mcpi-reborn-extended.gpg
