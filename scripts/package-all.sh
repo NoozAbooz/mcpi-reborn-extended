7d828ff5f7ab746723200eefcf47708f59128d13
mobilegmYT <zhengm58@gmail.com>
2022-03-13 10:37:47 -0600

    Automated commit

--- scripts/package-all.sh
+++ scripts/package-all.sh
@@ -14,3 +14,36 @@ rm -rf out
 
 ./scripts/package.sh client armhf
 ./scripts/package.sh server armhf
+
+# Package Debs
+VERSION="$(cat VERSION)"
+
+package() {
+    local dir="out/$1"
+
+    # Create DEBIAN Dir
+    rm -rf "${dir}/DEBIAN"
+    mkdir -p "${dir}/DEBIAN"
+    cp "debian/$1" "${dir}/DEBIAN/control"
+
+    # Format DEBIAN/control
+    sed -i "s/\${VERSION}/${VERSION}/g" "${dir}/DEBIAN/control"
+
+    # Fix Permissions On Jenkins
+    sudo chmod -R g-s "${dir}"
+
+    # Package
+    dpkg-deb --root-owner-group --build "${dir}" out
+}
+
+# Find And Package
+for dir in out/*; do
+    # Check If Directory Exists
+    if [ -d "${dir}" ]; then
+        # Check If Debian Package Exists
+        pkg="$(basename ${dir})"
+        if [ -f "debian/${pkg}" ]; then
+            package "${pkg}"
+        fi
+    fi
+done
