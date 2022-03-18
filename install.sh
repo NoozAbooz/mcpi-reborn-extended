8fbc5a7e22f835bce3aceb4f967df13d7d797b33
Michael Zheng <44128563+mobilegmYT@users.noreply.github.com>
2022-03-18 13:35:47 -0600

    Refactor install script

--- install.sh
+++ install.sh
@@ -1,82 +1,107 @@
 #!/bin/bash
 
+MCPI_DIR="${HOME}/.minecraft-pi"
+LIST_URL="https://github.com/mobilegmYT/mcpi-reborn-extended/raw/main/debs/mcpi-reborn-extended.list"
+LIST_PATH="/etc/apt/sources.list.d/mcpi-reborn-extended.list"
+KEY_URL="https://github.com/mobilegmYT/mcpi-reborn-extended/raw/main/debs/KEY.gpg"
+KEY_PATH="/tmp/mcpi-reborn-extended.gpg"
+MCPI_PY_URL="https://gist.github.com/mobilegmYT/78f50d3b80924d0c18ed818552254695/raw/a80ead7d30edf16327622002466f7b7e7df69aa6/minecraft.py"
+MCPI_PY_PATH="/usr/lib/python3/dist-packages/mcpi/minecraft.py"
+SOUND_URL="https://archive.org/download/libminecraftpe0.6.1/libminecraftpe06%2B08.so"
+SOUND_PATH="${MCPI_DIR}/overrides/libminecraftpe.so"
+TEXTURES_PREFIX="converted"
+TEXTURES_URL="https://cdn.discordapp.com/attachments/740287938453045401/944751207644278864/${TEXTURES_PREFIX}.zip"
+TMP_TEXTURES_PATH="/tmp/mcpi-reborn-textures.zip"
+TEXTURES_PATH="${MCPI_DIR}/overrides/images"
+MOB_TEXTURES_PATH="${TEXTURES_PATH}/mob"
+FIXER_PATH="/tmp/mcpi-skin-fixer.py"
+TMP_SKIN_PATH="${MCPI_DIR}/skin-mcpi.png"
+
 # Define functions
-function error() {
-  echo -e "$(tput setaf 1)$(tput bold)$1$(tput sgr 0)"
-  exit 1
+info() {
+  echo -e "$(tput smul)$(tput setaf 6)$(tput bold)$1$(tput sgr0)"
 }
 
-function warning() {
-  echo -e "$(tput setaf 1)$(tput bold)$1$(tput sgr 0)"
+warning() {
+  echo -e "$(tput setaf 3)$(tput bold)$1$(tput sgr0)"
   sleep 2
   true
 }
 
+error() {
+  echo -e "$(tput setaf 1)$(tput bold)$1$(tput sgr0)"
+  exit 1
+}
+
+priv_wget() {
+  sudo wget "$1" -O "$2" || error "Couldn't download '$2' from '$1'"
+}
+
+skin_dialog() {
+  zenity --file-selection --file-filter="Picture files (png) | *.png" --file-filter="All files | *" \
+    --title="Select your custom Java/Bedrock skin file (must be 4px). If you want to use the default skin, click cancel."
+}
+
 # Install depends
-if [ $(dpkg-query -W -f='${Status}' wget 2>/dev/null | grep -c "ok installed") -eq 0 ] || [ $(dpkg-query -W -f='${Status}' gnupg2 2>/dev/null | grep -c "ok installed") -eq 0 ] || [ $(dpkg-query -W -f='${Status}' unzip 2>/dev/null | grep -c "ok installed") -eq 0 ] || [ $(dpkg-query -W -f='${Status}' zenity 2>/dev/null | grep -c "ok installed") -eq 0 ];
-then
-  echo -e "\e[4m\e[21m\e[5mInstalling dependencies...\e[0m\e[97m"
-  sudo apt install -y wget gnupg unzip zenity || error "Failed to install dependencies, see error above"
-fi
+echo -e "\e[4m\e[21m\e[5mInstalling dependencies...\e[0m\e[97m"
+sudo apt install -y wget gnupg unzip zenity || error "Failed to install dependencies, see error above"
 
 # Install repo
-echo -e "\e[4m\e[21m\e[5mInstalling APT repository...\e[0m\e[97m"
-sudo wget -O /etc/apt/sources.list.d/mcpi-reborn-extended.list https://github.com/mobilegmYT/mcpi-reborn-extended/raw/main/debs/mcpi-reborn-extended.list
+info "Installing APT repository..."
+priv_wget "${LIST_URL}" "${LIST_PATH}"
 
 # Install GPG key
-sudo wget https://github.com/mobilegmYT/mcpi-reborn-extended/raw/main/debs/KEY.gpg
-sudo apt-key add KEY.gpg
-sudo rm KEY.gpg
-#sudo mv KEY.gpg /etc/apt/trusted.gpg.d/mcpi-reborn-extended.gpg
+priv_wget "${KEY_URL}" "${KEY_PATH}"
+sudo apt-key add "${KEY_PATH}"
 
 sudo apt update --allow-releaseinfo-change || warning "Failed to run 'sudo apt update'! Please run that command manually"
 
 # Nuke vanilla reborn if installed
-if [ $(dpkg-query -W -f='${Status}' minecraft-pi-reborn-client 2>/dev/null | grep -c "ok installed") -eq 1 ]
-then
-  echo -e "\e[4m\e[21m\e[5mRemoving vanilla reborn to resolve conflicts...\e[0m\e[97m"
-  sudo apt-get remove -y minecraft-pi-reborn-client || warning "Could not reinstall reborn to switch to extended version! Please do it manually"
-  sudo apt-get install -y minecraft-pi-reborn-client gmcpil
-fi
+info "Removing vanilla reborn to resolve conflicts..."
+sudo apt-get remove -y minecraft-pi-reborn-client || warning "Could not reinstall reborn to switch to extended version! Please do it manually"
+sudo apt-get install -y minecraft-pi-reborn-client gmcpil
 
 # Install Python API
-if [ $(dpkg-query -W -f='${Status}' python3-minecraftpi 2>/dev/null | grep -c "ok installed") -eq 0 ]
-then
-  echo  -e "\033[33;5mInstalling Python API...\033[0m"
-  sudo apt install -y python3-minecraftpi || warning "Could not install Python API"
-  wget https://gist.github.com/mobilegmYT/78f50d3b80924d0c18ed818552254695/raw/a80ead7d30edf16327622002466f7b7e7df69aa6/minecraft.py
-  sudo mv minecraft.py /usr/lib/python3/dist-packages/mcpi/minecraft.py
-fi
+info "Installing Python API..."
+sudo apt install -y python3-minecraftpi || warning "Could not install Python API"
+priv_wget "${MCPI_PY_URL}" "${MCPI_PY_PATH}"
+
+mkdir -p "${MOB_TEXTURES_PATH}"
 
 # Install sound
-echo  -e "\033[33;5mInstalling sound files...\033[0m"
-wget https://archive.org/download/libminecraftpe0.6.1/libminecraftpe06%2B08.so
-mkdir -p ~/.minecraft-pi/overrides
-mv "libminecraftpe06+08.so" ~/.minecraft-pi/overrides/libminecraftpe.so
+info "Installing sound files..."
+wget "${SOUND_URL}" -O "${SOUND_PATH}"
 
 # Install 1.18 textures
-echo  -e "\033[33;5mInstalling 1.18 textures...\033[0m"
-wget https://cdn.discordapp.com/attachments/740287938453045401/944751207644278864/converted.zip
-unzip converted.zip
-mkdir -p ~/.minecraft-pi/overrides/images
-mv converted/* ~/.minecraft-pi/overrides/images/
-rm -rf converted/ converted.zip
+info "Installing 1.18 textures..."
+(
+  set -e
+  cd /tmp
+  wget "${TEXTURES_URL}" -O "${TMP_TEXTURES_PATH}"
+  unzip "${TMP_TEXTURES_PATH}"
+  mv "${TEXTURES_PREFIX}/"* "${TEXTURES_PATH}"
+)
 
 # Add custom skin
-echo  -e "\033[33;5mEnabling custom skins...\033[0m"
+info "Enabling custom skins..."
+pip install pillow
 pip3 install pillow
-skinpath=$(zenity --file-selection --title='Select your custom Java/Bedrock skin file (must be 4px). If you want to use the default skin, click cancel' --file-filter='Picture files (png) | *.png' --file-filter='All files | *')
-echo "Using skin located at:" $skinpath
-    
-cp $skinpath ~/.minecraft-pi/skin-mcpi.png
-mkdir -p ~/.minecraft-pi/overrides/images/mob
+SKIN_PATH="$(skin_dialog)"
+echo "Using skin located at '${SKIN_PATH}'"
+
+cp "${SKIN_PATH}" "${TMP_SKIN_PATH}"
     
 echo "Applying texture size patch..."
-wget https://bitbucket.org/MattHawkinsUK/rpispy-misc/raw/master/minecraft/minecraft_skin_fixer.py
-sed -i "s|/opt/minecraft-pi/data/images/mob/char.png|~/.minecraft-pi/skin-mcpi.png|g" minecraft_skin_fixer.py
-sudo python3 minecraft_skin_fixer.py
+(
+  set -e
+  cd /tmp
+  wget "https://bitbucket.org/MattHawkinsUK/rpispy-misc/raw/master/minecraft/minecraft_skin_fixer.py" -O "${FIXER_PATH}"
+  sed -i "s|/opt/minecraft-pi/data/images/mob/char.png|${TMP_SKIN_PATH}|g" "${FIXER_PATH}"
+  sudo python3 "${FIXER_PATH}"
+)
     
-mv ~/.minecraft-pi/skin-mcpi.png ~/.minecraft-pi/overrides/images/mob/char.png
-rm minecraft_skin_fixer.py
+mv "${FIXER_PATH}" "${MOB_TEXTURES_PATH}/char.png"
 
-
+# Finish
+echo -e "\n"
+info "Installation complete! Refer to https://github.com/mobilegmYT/mcpi-reborn-extended#user-guide for usage instructions."
