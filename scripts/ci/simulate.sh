7759c6ae0265bf5bee309d900319bdb8155a66a1
NoozSBC <zhengm58@gmail.com>
2023-02-14 23:05:32 -0700

    2.4.6-1

--- scripts/ci/simulate.sh
+++ scripts/ci/simulate.sh
@@ -3,6 +3,4 @@
 set -e
 
 # Run
-#docker run --rm -v "$(pwd):/data" -w "/data" debian:buster-slim sh -c "./scripts/ci/run.sh"
-
-docker run --rm -v "$(pwd):/data" -w "/data" noozsbc/mcpi-reborn-extended-build:latest sh -c "./scripts/ci/run.sh"
+docker run --rm -v "$(pwd):/data" -w "/data" buildpack-deps:bullseye sh -c "./scripts/ci/run.sh"
