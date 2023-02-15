32ff129e824b3452f49989def1562fe7c77fa9b4
NoozSBC <zhengm58@gmail.com>
2023-02-14 21:45:30 -0700

    2.4.6-1 idk what's happening i hate everything

--- scripts/package-all.sh
+++ scripts/package-all.sh
@@ -5,6 +5,9 @@ set -e
 # Clean Prefix
 rm -rf out
 
+# Fix ownership
+git config --global --add safe.directory ./dependencies/*
+
 # Build
 ./scripts/package.sh client amd64
 ./scripts/package.sh server amd64
