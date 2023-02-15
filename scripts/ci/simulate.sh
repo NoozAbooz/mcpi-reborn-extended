9ff1acf02f4c5f69eee4a20b6899fbdd419d5935
NoozSBC <zhengm58@gmail.com>
2023-02-14 21:37:22 -0700

    2.4.6-1 Try #2

--- scripts/ci/simulate.sh
+++ scripts/ci/simulate.sh
@@ -3,4 +3,5 @@
 set -e
 
 # Run
-docker run --rm -v "$(pwd):/data" -w "/data" debian:buster-slim sh -c "./scripts/ci/run.sh"
+#docker run --rm -v "$(pwd):/data" -w "/data" debian:buster-slim sh -c "./scripts/ci/run.sh"
+docker run --rm -v "$(pwd):/data" -w '/data' -u "$(id -u):$(id -g)" noozsbc/minecraft-pi-reborn-build:latest ./scripts/ci/run.sh
