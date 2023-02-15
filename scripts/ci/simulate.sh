f5ab7c86e691ce273564423ad188cbb7e0bf76d2
NoozSBC <zhengm58@gmail.com>
2023-02-14 21:53:04 -0700

    2.4.6-1 amoguzzy

--- scripts/ci/simulate.sh
+++ scripts/ci/simulate.sh
@@ -5,4 +5,4 @@ set -e
 # Run
 #docker run --rm -v "$(pwd):/data" -w "/data" debian:buster-slim sh -c "./scripts/ci/run.sh"
 
-docker run --rm -v "$(pwd):/data" -w '/data' -u "$(id -u):$(id -g)" noozsbc/mcpi-reborn-extended-build:latest ./scripts/ci/run.sh
+docker run --rm -v "$(pwd):/data" -w "/data" noozsbc/mcpi-reborn-extended-build:latest sh -c "./scripts/ci/run.sh"
