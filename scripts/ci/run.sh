ecdd9f1a641c50223c849fe7fce6771a2c1d0183
NoozSBC <zhengm58@gmail.com>
2023-02-14 23:08:37 -0700

    2.4.6-1 mb forgor depends

--- scripts/ci/run.sh
+++ scripts/ci/run.sh
@@ -3,12 +3,11 @@
 set -e
 
 # Get Depends
-#echo '==== Installing Dependencies ===='
-#./scripts/install-dependencies.sh amd64 armhf arm64
+echo '==== Installing Dependencies ===='
+./scripts/install-dependencies.sh amd64 armhf arm64
 
 # Build/Package
 echo '==== Building & Packaging ===='
-
 ./scripts/package-all.sh
 
 # Test
